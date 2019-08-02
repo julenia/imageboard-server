2ac97a6f20b0c93c41da939649e961bf87b6d304
julenia <julia.jankowska@onet.eu>
2019-08-02 14:33:44 +0200

    Force schema rebuild

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const databaseUrl = 'postgres://postgres:secret@localhost:5432/postgres'
 
 const db = new Sequelize(process.env.DATABASE_URL || databaseUrl)
 
-db.sync()
+db.sync({ force: true })
 .then(()=> console.log('database connected'))
 .error(err => console.error(err))
 
