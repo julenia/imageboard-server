4a363b9c2799ac7a0f44e39c63408e28e5309b66
julenia <julia.jankowska@onet.eu>
2019-08-02 16:27:17 +0200

    authentication

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 4000
 const db = require('./db')
+const jwtRouter = require('./auth/router')
 const imageRouter = require('./image/router')
+const userRouter = require('./user/router')
 const corsMiddleware = cors()
 app.use(corsMiddleware)
 const parserMiddleware = bodyParser.json()
 app.use(parserMiddleware)
 
 app.use(imageRouter)
-
+app.use(jwtRouter)
+app.use(userRouter)
 app.listen(port, ()=> console.log(`Listen on port: ${port}`))
